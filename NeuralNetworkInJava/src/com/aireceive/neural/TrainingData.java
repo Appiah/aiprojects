261bce0ac8bf0fe26d5367662ce9df8f1579fbbd
Appiah <emap218@gmail.com>
2024-06-12 16:42:48 +0000

    Pushing the NeuralNetworkInJava alone, then would add the rest progressively.

---
+++ NeuralNetworkInJava/src/com/aireceive/neural/TrainingData.java
@@ -0,0 +1,13 @@
+package com.aireceive.neural;
+
+public class TrainingData {
+ 
+    float[] data;
+    float[] expectedOutput;
+   
+    public TrainingData(float[] data, float[] expectedOutput) {
+        this.data = data;
+        this.expectedOutput = expectedOutput;
+    }
+   
+}
